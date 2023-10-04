7d804cc76ebacb123969a39c16c76f3ff195dabf
ManuuelG <manuelguerreron93@gmail.com>
2023-10-04 18:36:56 +0200

    constantino

--- scripts.js
+++ scripts.js
@@ -35,7 +35,7 @@ const pregunta = {
 }
 
 function imprimePregunta(pregunta) {
-  const newHTML = "";
+  let newHTML = "";
 
   newHTML += imprimeTitulo(pregunta);
   newHTML += imprimeTodasLasRespuestas(pregunta);
@@ -45,22 +45,52 @@ function imprimePregunta(pregunta) {
 
 function imprimeTitulo(pregunta) {
   // Put your code here
+
+  return `<p>${pregunta.titulo}</p>`
 }
+
+let respuesta = pregunta.respuestas
 
 function imprimeTodasLasRespuestas(pregunta) {
   // Put your code here
+let pepito = ""
+  for (let i = 0; i < respuesta.length; i++){
+   pepito += imprimeUnaRespuesta(respuesta[i])
+  } 
+  return pepito
 }
 
 function imprimeUnaRespuesta(respuesta) {
   // Put your code here
-}
+
+
+let answer = "";
+
+answer += imprimeLabel(respuesta);
+answer += imprimeInput(respuesta);
+
+
+  return answer;
+ }
+
+
 
 function imprimeLabel(respuesta) {
   // Put your code here
+
+let label = ""
+label += `<label for = "${respuesta.id}"> ${respuesta.label}</label>`
+return label
+
 }
 
 function imprimeInput(respuesta) {
   // Put your code here
+
+  let input = ""
+  input += `<input id= ${respuesta.id} name= ${respuesta.name} type="radio" value= ${respuesta.value}>`
+  return input
 }
+
 
 document.getElementById("contenedorPreguntas").innerHTML = imprimePregunta(pregunta);
